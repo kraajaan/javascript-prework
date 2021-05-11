7411e8fd46503124fb4901ad4d25970c8e4c7f09
Mateusz Krajewski <admin@cray.lh.pl>
2021-05-11 22:58:02 +0200

    Odczyt ruchu komputera

--- js/script.js
+++ js/script.js
@@ -1,4 +1,21 @@
-let computerMove = 'kamień';
-let playerMove = 'papier':
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+//let randomNumber2 = Math.floor(Math.random() * 9 + 11);
+//printMessage('Los z zakresu 11-19: ' + randomNumber2);
+
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if(randomNumber == 2){
+  computerMove = 'papier';
+}
+else if(randomNumber == 3){
+  computerMove = 'nożyce';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
